fe2ee4cd67ff6fe1c6938daf01a971d75fbbf444
Ahiga <39hige1@gmail.com>
2021-02-25 23:31:57 +0100

    Add files via upload

--- cogs/condom.py
+++ cogs/condom.py
@@ -5,12 +5,29 @@ import os
 import aiohttp
 import random
 import re
+import sqlite3
 
 from discord.ext import commands
 from discord.ext.commands import errors
 from utils import default
 from utils import lists, permissions, http, default
 from discord import Webhook, AsyncWebhookAdapter
+
+from profanityfilter import ProfanityFilter
+
+from nudenet import NudeDetector, NudeClassifier
+
+from io import StringIO, BytesIO
+
+from PIL import Image
+
+
+async def send_cmd_help(ctx):
+    if ctx.invoked_subcommand:
+        await ctx.send_help(str(ctx.invoked_subcommand))
+    else:
+        await ctx.send_help(str(ctx.command))
+
 
 class Condom(commands.Cog):
     def __init__(self, bot):
@@ -21,10 +38,18 @@ class Condom(commands.Cog):
     @commands.Cog.listener()
     @commands.guild_only()
     async def on_message(self, message):
+        if message.author.bot:
+                return
+        author_id = str(message.author.id)
+        guild_id = str(message.guild.id)
+        #print("message")
         userinp = message.content
         message.content = message.content.lower().replace('', '')
         user = message.author
         channelid = message.channel.name
+        if "discordapp.com/attachments" in message.content:
+            return
+
         if "tenor." in message.content:
             await message.delete();
 
@@ -52,32 +77,116 @@ class Condom(commands.Cog):
             await message.delete();
             return
 
+        if message.content == 'f':
+            hearts = ['❤', '💛', '💚', '💙', '💜']
+            await message.channel.send(f"**{message.author.name}** has paid their respect {random.choice(hearts)}")
+
         if channelid != "degeneral":
             return
 
-        async def niggerfuckk():
-            if message.author.bot:
-                return
-            clean = message.content.replace(f"{word1}", f"{word2}")
-            async with aiohttp.ClientSession() as session:
-                webhook = Webhook.from_url('https://discordapp.com/api/webhooks/661116894249484289/1YhybxOsoR2HnBPvCtUsORrkRulgc8ENmVojDMYcLX5Ukg1yI4eaitfTmm2w5JNvHUtK', adapter=AsyncWebhookAdapter(session))
-                username = message.author.display_name
-                pfp = message.author.avatar_url_as(size=1024)
-                await message.delete()
-                await webhook.send(f'{clean}', username=str(username), avatar_url=str(pfp))
+        image_types = ['jpg','png','jpeg', 'gif']
+        for attachment in message.attachments:
+            if any(attachment.filename.lower().endswith(image) for image in image_types):
+                #classifier = NudeClassifier()
+                detector = NudeDetector()
+                await attachment.save("/home/ahigaaa/Laikabotfuck/cogs/temp/img.jpg")
+                a = str(detector.detect('/home/ahigaaa/Laikabotfuck/cogs/temp/img.jpg'))
+                channel = self.bot.get_channel(660985458049941524)
+                await channel.send(f'image saved')
+                await channel.send(f'{a}')
+                if 'EXPOSED_GENITALIA_F' in a or 'EXPOSED_GENITALIA_M' in a or 'EXPOSED_BREAST_F' in a:
+                    DB_NAME = "database"
+                    db_path = os.path.abspath("/home/ahigaaa/Laikabotfuck/db/" + DB_NAME + ".db")
+                    self.db = sqlite3.connect(db_path)
+                    self.db_cursor = self.db.cursor()
+                    print(f"Connected")
+                    print("true")
+
+                    isImage=True
+                    if isImage:
+                        detector.censor(
+                            '/home/ahigaaa/Laikabotfuck/cogs/temp/img.jpg', 
+                            out_path='/home/ahigaaa/Laikabotfuck/cogs/temp/img_censored.jpg', 
+                            visualize=False
+                        )
+
+                        async with aiohttp.ClientSession() as session:
+                            webhook = Webhook.from_url('https://discordapp.com/api/webhooks/661116894249484289/1YhybxOsoR2HnBPvCtUsORrkRulgc8ENmVojDMYcLX5Ukg1yI4eaitfTmm2w5JNvHUtK', adapter=AsyncWebhookAdapter(session))
+                            username = message.author.display_name
+                            pfp = message.author.avatar_url_as(size=1024)
+                            file = discord.File("/home/ahigaaa/Laikabotfuck/cogs/temp/img_censored.jpg", filename="image.jpg")
+                            await webhook.send(file=file, username=str(username), avatar_url=str(pfp))
+                            await message.delete()
+                            self.db_cursor.execute(f"SELECT * FROM fucku WHERE UserID='{author_id}' AND GuildID='{guild_id}'")
+                            user = self.db_cursor.fetchone()
+                            if user:
+                                print("User found")
+                                sql = ("UPDATE fucku SET Credit=? WHERE UserID=? AND GuildID=?")
+                                val = (int(user[4] + 2), int(message.author.id), int(message.guild.id))
+                                self.db_cursor.execute(sql, val)
+                                self.db.commit()
+                            try:
+                                self.db_cursor.close()
+                                self.db.close()
+                            except Exception as e:
+                                return
+
 
         async def niggerfuck():
             if message.author.bot:
                 return
+            DB_NAME = "database"
+            db_path = os.path.abspath("/home/ahigaaa/Laikabotfuck/db/" + DB_NAME + ".db")
+            self.db = sqlite3.connect(db_path)
+            self.db_cursor = self.db.cursor()
+            print(f"Connected")
+
             a = message.content
-            b = {'mhm': 'romet is a x-dressing faggot kil myself', 'faith': 'fatih',
-            'excluded': 'excludedfag', 'indica': 'ethot', 'pozu': 'kike', 'tealeaf': '3 FREE STONKS', 'alex jones': 'fat james mason',
-            'boomer': 'hoomer', 'jesus christ': 'dead kike on a spike', 'jesus': 'jewsus', 'vocaloid': 'mongoloid', 'tranny cock': 'faggot',
-            'shota': 'faggot', 'sissy': 'faggot', 'brap': 'toot', 'nya': 'catfucker', 'femboy': 'femsnot', 'femboi': 'femsnot', 'boypussy': 'onajole',
-            'boipussy': 'onajole', 'boy pussy': 'onajole', 'boi pussy': 'onajole', 'systemspace': 'systemgay', 'deal': 'chungus', 'chink': 'glorious chinese',
-            'thanks giving': 'indigenous peoples day', 'thanksgiving': 'indigenous peoples day', 'columbus day': 'indigenous peoples day'}
+            b = lists.censorship
+
+            if "chinese" in a:
+                pf = ProfanityFilter()
+                if pf.is_profane(f"{a}") == True:
+                    word1 = "chinese"
+                    word2 = "GLORIOUS CHINESE"
+                    a = a.replace(f"{word1}", f"{word2}")
+            if "china" in a:
+                pf = ProfanityFilter()
+                if pf.is_profane(f"{a}") == True:
+                    word1 = "china"
+                    word2 = "CHINA IS GLORIOUS RIDE DA TIGA I LOVE CHINA"
+                    a = a.replace(f"{word1}", f"{word2}")
+            if "black woman" in a:
+                answer = random.choice(lists.niggerresponse)
+                word1 = "black woman"
+                word2 = str(answer)
+                a = a.replace(f"{word1}", f"{word2}")
+            if "black people" in a:
+                answer = random.choice(lists.niggerresponse)
+                word1 = "black people"
+                word2 = str(answer)
+                a = a.replace(f"{word1}", f"{word2}")
+            if "black person" in a:
+                answer = random.choice(lists.niggerresponse)
+                word1 = "black person"
+                word2 = str(answer)
+                a = a.replace(f"{word1}", f"{word2}")
+            if "african" in a:
+                answer = random.choice(lists.niggerresponse)
+                word1 = "african"
+                word2 = str(answer)
+                a = a.replace(f"{word1}", f"{word2}")
+            if "blacks" in a:
+                if "blacksmith" in a:
+                    return
+                answer = random.choice(lists.niggerresponse)
+                word1 = "blacks"
+                word2 = str(answer) + "s"
+                a = a.replace(f"{word1}", f"{word2}")
+
             for x,y in b.items():
                 a = a.replace(x, y)
+
             async with aiohttp.ClientSession() as session:
                 webhook = Webhook.from_url('https://discordapp.com/api/webhooks/661116894249484289/1YhybxOsoR2HnBPvCtUsORrkRulgc8ENmVojDMYcLX5Ukg1yI4eaitfTmm2w5JNvHUtK', adapter=AsyncWebhookAdapter(session))
                 username = message.author.display_name
@@ -85,8 +194,21 @@ class Condom(commands.Cog):
                 await message.delete()
                 await webhook.send(f'{a}', username=str(username), avatar_url=str(pfp))
 
+                self.db_cursor.execute(f"SELECT * FROM fucku WHERE UserID='{author_id}' AND GuildID='{guild_id}'")
+                user = self.db_cursor.fetchone()
+                if user:
+                    print("User found")
+                    sql = ("UPDATE fucku SET Credit=? WHERE UserID=? AND GuildID=?")
+                    val = (int(user[4] - 3), int(message.author.id), int(message.guild.id))
+                    self.db_cursor.execute(sql, val)
+                    self.db.commit()
+                try:
+                    self.db_cursor.close()
+                    self.db.close()
+                except Exception as e:
+                    return
 
-        if "@everyone" in message.content or "@here" in message.content or "72339619" in message.content or "waa" in message.content or "705050082818523195.png" in message.content or "72340365141409" in message.content:
+        if "@everyone" in message.content or "@here" in message.content:
             if discord.utils.get(user.roles, name="Heimdallar") != None:
                     return
             else:
@@ -97,83 +219,37 @@ class Condom(commands.Cog):
                 return
             await message.add_reaction('<:cringe:661301648580280359>')
 
+        elif "pornhub.com/" in message.content:
+            await message.channel.send(f'Meet someone in real lyfe, virgin xd')
 
-        elif "bot" in message.content or "laika" in message.content:
+        elif ":trump" in message.content or "kenya" in message.content or "maya" in message.content or "sanya" in message.content or "tanya" in message.content or "chechnya" in message.content or "thnyan" in message.content or ":nyan:" in message.content or "nyaggot" in message.content or "https://" in message.content or "indication" in message.content or "excludedfag" in message.content or "blacksmith" in message.content:
+            return
+
+        wordlist = ("twink", "serb", "trump", "mhm", "faith", "excluded", "indica", "pozu", "tealeaf", "alex jones", "boomer", "jesus", "vocaloud", "tranny cock", "shota", "sissy", "brap", "nya", "femboy", "femboi", "boypussy", "boy pussy", "boipussy", "boi pussy", "systemspace", "deal", "chink", "thanks giving", "thanksgiving", "columbus day")
+        for i in wordlist:
+            if i in message.content:
+                await niggerfuck()
+
+        if "bot" in message.content or "laika" in message.content:
             if "both" in message.content:
                 return
             pf = ProfanityFilter()
             if "kill" in message.content or "hate" in message.content:
                 answer = random.choice(lists.anger)
-                if message.author.bot:
-                    return
                 await message.channel.send(f'{answer}')
             if pf.is_profane(f"{message.content}") == True:
                 answer = random.choice(lists.anger)
-                if message.author.bot:
-                    return
                 await message.channel.send(f'{answer}')
             elif pf.is_clean(f"{message.content}") == True:
                 return
-        elif "kill myself" in message.content:
+        if "kill myself" in message.content:
             answer = random.choice(lists.suicide)
-            if message.author.bot:
-                return
             await message.channel.send(f'{answer}')
-        elif "fix your bot" in message.content:
+        if "fix your bot" in message.content:
             answer = random.choice(lists.anger)
-            if message.author.bot:
-                return
             await message.channel.send(f'{answer}')
 
-        if "chinese" in message.content:
-            pf = ProfanityFilter()
-            if pf.is_profane(f"{message.content}") == True:
-                word1 = "chinese"
-                word2 = "GLORIOUS CHINESE"
-                await niggerfuckk()
 
-        elif "china" in message.content:
-            pf = ProfanityFilter()
-            if pf.is_profane(f"{message.content}") == True:
-                word1 = "china"
-                word2 = "CHINA IS GLORIOUS RIDE DA TIGA I LOVE CHINA"
-                await niggerfuckk()
-
-        elif "black woman" in message.content:
-            answer = random.choice(lists.niggerresponse)
-            word1 = "black woman"
-            word2 = str(answer)
-            await niggerfuckk()
-        elif "black people" in message.content:
-            answer = random.choice(lists.niggerresponse)
-            word1 = "black people"
-            word2 = str(answer)
-            await niggerfuckk()
-        elif "black person" in message.content:
-            answer = random.choice(lists.niggerresponse)
-            word1 = "black person"
-            word2 = str(answer)
-            await niggerfuckk()
-        elif "african" in message.content:
-            answer = random.choice(lists.niggerresponse)
-            word1 = "african"
-            word2 = str(answer)
-            await niggerfuckk()
-        elif "blacks" in message.content:
-            if "blacksmith" in message.content:
-                return
-            answer = random.choice(lists.niggerresponse)
-            word1 = "blacks"
-            word2 = str(answer) + "s"
-            await niggerfuckk()
-
-        elif "kenya" in message.content or "maya" in message.content or "sanya" in message.content or "tanya" in message.content or "chechnya" in message.content or "thnyan" in message.content or ":nyan:" in message.content or "nyaggot" in message.content or "https://" in message.content or "indication" in message.content or "excludedfag" in message.content or "blacksmith" in message.content:
-            return
-
-        wordlist = ("mhm", "faith", "excluded", "indica", "pozu", "tealeaf", "alex jones", "boomer", "jesus", "vocaloud", "tranny cock", "shota", "sissy", "brap", "nya", "femboy", "femboi", "boypussy", "boy pussy", "boipussy", "boi pussy", "systemspace", "deal", "chink", "thanks giving", "thanksgiving", "columbus day")
-        for i in wordlist:
-            if i in message.content:
-                await niggerfuck()
 
 def setup(bot):
     bot.add_cog(Condom(bot))
